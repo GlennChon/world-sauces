a981d73b2c413201c42c1c7aff671af732c53794
GlennChon <glenn.chon@gmail.com>
2019-08-16 16:00:31 +0700

    initial commit

---
+++ src/components/searchBar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import "../css/searchBar.css";
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ searchValue: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleSearchClick(this.state.searchValue);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <form className="search-wrapper" onFormSubmit={this.handleSubmit}>
+          <input
+            type="search"
+            placeholder="Sauce Search"
+            onChange={this.handleChange}
+            value={this.state.searchValue}
+          />
+          <button
+            type="submit"
+            className="search-btn"
+            onClick={this.handleSubmit}
+          >
+            Search
+          </button>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default SearchBar;
