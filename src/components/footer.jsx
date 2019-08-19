71b23f175ce2f5646270bf4cb96eea1a948fe529
GlennChon <glenn.chon@gmail.com>
2019-08-19 22:03:32 +0700

    footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Footer extends Component {
+  state = {};
+  render() {
+    return (
+      <footer>
+        <h1>footer</h1>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
