2a85a92ae8082bf35b751ac9901717a675a327d4
GlennChon <glenn.chon@gmail.com>
2019-08-19 22:03:16 +0700

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import WebFont from "webfontloader";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 WebFont.load({
   google: {
     families: [
