a77ebe4490654a8e80a2f9855372541dc0e9ec06
GlennChon <glenn.chon@gmail.com>
2019-08-19 22:03:41 +0700

    header component

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <header>
+        <h1>Header</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
