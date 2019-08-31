5b04cb13251b69734d0fe175037dc1967063a793
GlennChon <glenn.chon@gmail.com>
2019-08-31 19:55:08 +0700

    basic recipe component

---
+++ src/components/recipe.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import RecipeCard from "./recipeCard";
+
+class Recipe extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <RecipeCard />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Recipe;
