69a3b679c3d3b725d50d87131bc519f3ab6eabbf
GlennChon <glenn.chon@gmail.com>
2019-09-01 13:35:35 +0700

    added fields

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,7 +2,22 @@ import React, { Component } from "react";
 
 class LoginForm extends Component {
   render() {
-    return <h1>Login</h1>;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form action="">
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
   }
 }
 
