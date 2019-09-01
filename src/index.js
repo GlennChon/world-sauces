36a0a899358ff7e5219d641668614c983f597b5a
GlennChon <glenn.chon@gmail.com>
2019-09-01 16:41:15 +0700

    added browser router to handle  routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import WebFont from "webfontloader";
-import "bootstrap/dist/css/bootstrap.min.css";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.css";
 import "./css/recipes.css";
 import "./index.css";
 
@@ -20,7 +22,12 @@ WebFont.load({
     ]
   }
 });
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
