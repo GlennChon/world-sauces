d26e4a4ed8c3dc3a6cb92e1bcbcdbaf761c669be
GlennChon <glenn.chon@gmail.com>
2019-09-01 13:35:59 +0700

    commented out  missing components

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import { Route, Redirect, Switch } from "react-router-dom";
 // Services
 import auth from "./services/authService";
 // Components
-import Logo from "./components/logo";
-import Users from "./components/users";
+// import Logo from "./components/logo";
+// import Users from "./components/users";
 import NavBar from "./components/navBar";
-import Recipe from "./components/recipe";
-import Footer from "./components/footer";
-import Logout from "./components/logout";
-import Recipes from "./components/recipes";
+// import Recipe from "./components/recipe";
+// import Footer from "./components/footer";
+// import Logout from "./components/logout";
+// import Recipes from "./components/recipes";
 import NotFound from "./components/notFound";
-import Countries from "./components/countries";
-import SearchBar from "./components/searchBar";
+// import Countries from "./components/countries";
+// import SearchBar from "./components/searchBar";
 import LoginForm from "./components/loginForm";
-import RecipeForm from "./components/recipeForm";
-import RegisterForm from "./components/registerForm";
-import TasteProfiles from "./components/tasteProfiles";
+// import RecipeForm from "./components/recipeForm";
+// import RegisterForm from "./components/registerForm";
+// import TasteProfiles from "./components/tasteProfiles";
 // CSS
 import "./css/App.css";
 import "react-toastify/dist/ReactToastify.css";
@@ -40,13 +40,6 @@ class App extends Component {
         <main className="container">
           <Switch>
             <Route path="/login" component={LoginForm} />
-            <Route path="/logOut" component={LogOut} />
-            <Route path="/recipes/:id" component={RecipeForm} />
-            <Route path="/recipes" component={Recipes} />
-            <Route path="/profiles/:id" component={ProfileForm} />
-            <Route path="/profiles" component={Profiles} />
-            <Route path="/taste-profiles" component={TasteProfiles} />
-            <Route path="/countries" component={Countries} />
             <Route path="/not-found" component={NotFound} />
             <Redirect from="/" exact to="/recipes" />
             <Redirect to="/not-found" />
@@ -58,3 +51,13 @@ class App extends Component {
 }
 
 export default App;
+
+/*
+            <Route path="/logOut" component={LogOut} />
+            <Route path="/recipes/:id" component={RecipeForm} />
+            <Route path="/recipes" component={Recipes} />
+            <Route path="/profiles/:id" component={ProfileForm} />
+            <Route path="/profiles" component={Profiles} />
+            <Route path="/taste-profiles" component={TasteProfiles} />
+            <Route path="/countries" component={Countries} />
+*/
