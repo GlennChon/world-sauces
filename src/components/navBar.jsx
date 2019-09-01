fad6e4a13967bcefd19912da5daa96d3da6b5588
GlennChon <glenn.chon@gmail.com>
2019-09-01 13:24:25 +0700

    added navbar elements

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,17 +1,58 @@
 import React, { Component } from "react";
-import Logo from "./logo";
+import { Link, NavLink } from "react-router-dom";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <ul>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </React.Fragment>
-    );
-  }
-}
+const NavBar = ({ user }) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        World Sauces
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/recipes">
+            Recipes
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/login">
+            Login
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/logout">
+            Logout
+          </NavLink>
+          {!user && (
+            <React.Fragment>
+              <NavLink className="nav-item nav-link" to="/login">
+                Login
+              </NavLink>
+              <NavLink className="nav-item nav-link" to="/register">
+                Register
+              </NavLink>
+            </React.Fragment>
+          )}
+          {user && (
+            <React.Fragment>
+              <NavLink className="nav-item nav-link" to="/profile">
+                {user.name}
+              </NavLink>
+              <NavLink className="nav-item nav-link" to="/logout">
+                Logout
+              </NavLink>
+            </React.Fragment>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
