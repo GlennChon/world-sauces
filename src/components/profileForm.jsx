657538caaa0211a6ea9a71398cedb219d5297dac
GlennChon <glenn.chon@gmail.com>
2019-09-02 23:20:20 +0700

    added blank form/page

---
+++ src/components/profileForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class ProfileForm extends Component {
+  state = {};
+  render() {
+    return <h1>Profile Form</h1>;
+  }
+}
+
+export default ProfileForm;
