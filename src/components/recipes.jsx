7da308b8b25d289c7bee7cc90bd85a84ec13cb57
GlennChon <glenn.chon@gmail.com>
2019-09-03 22:43:30 +0700

    add earch, country & recipe list to recipes page

--- src/components/recipes.jsx
+++ src/components/recipes.jsx
@@ -1,61 +1,123 @@
 import React, { Component } from "react";
-import { getRecipes } from "../services/fakeRecipeService";
+import { Link } from "react-router-dom";
+import _ from "lodash";
+
+import * as recipeService from "../services/recipeService";
+import { paginate } from "../utils/paginate";
+import Pagination from "./common/pagination";
+import { getCountries } from "../services/countryService";
+import SearchBar from "./common/searchBar";
+import RecipeList from "./recipeList";
 
 class Recipes extends Component {
   state = {
-    recipes: []
+    data: { selectValue: "" },
+    recipes: {},
+    countries: [],
+    currentPage: 1,
+    pageSize: 4,
+    searchQuery: "",
+    searchCountry: "",
+    searchAuthor: "",
+    searchSort: ""
   };
 
-  componentDidMount() {
-    this.loadRecipes();
+  async componentDidMount() {
+    await this.popularRecipes();
+    await this.populateCountries();
   }
 
-  loadRecipes = async () => {
-    let rcps = await getRecipes();
-    console.log(rcps);
+  populateCountries = async () => {
+    const countries = await getCountries();
+    this.setState({ countries: countries.data });
+  };
+
+  popularRecipes = async () => {
+    let searchSort = "likes-asc";
+    let json = await recipeService.getRecipes("", "", "", searchSort);
+    console.log(json.data);
+    this.setState({ recipes: json, searchSort });
+  };
+
+  handleLike = recipe => {
+    const recipes = [...this.state.recipes];
+    const index = recipes.indexOf(recipe);
+    recipes[index] = { ...recipes[index] };
+    recipes[index].liked = !recipes[index].liked;
+    this.setState({ recipes });
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  handleCountrySelect = country => {
     this.setState({
-      recipes: rcps
+      searchCountry: country
     });
   };
 
-  handleClick = recipe => {
-    console.log(recipe.title + " clicked");
+  getPagedData = () => {
+    const {
+      pageSize,
+      currentPage,
+      searchCountry,
+      searchQuery,
+      recipes
+    } = this.state;
+
+    let filtered = recipes;
+    if (searchQuery)
+      filtered = recipes.filter(r =>
+        r.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+    else if (searchCountry && searchCountry._id)
+      filtered = recipes.filter(
+        r => r._origin_country_code._id === searchCountry._id
+      );
+
+    const sorted = _.orderBy(filtered);
+
+    const movies = paginate(sorted, currentPage, pageSize);
+
+    return { totalCount: filtered.length, data: movies };
+  };
+
+  handleClick = i => {
+    console.log(this.state.recipes[i]);
+  };
+
+  searchRecipes = async (searchQuery, searchCountry) => {
+    if (!searchQuery) searchQuery = "";
+    if (!searchCountry || searchCountry === "Any") searchCountry = "";
+    let json = await recipeService.getRecipes(
+      searchQuery,
+      searchCountry,
+      "",
+      ""
+    );
+    this.setState({ recipes: json, searchQuery });
   };
 
   render() {
+    const { pageSize, currentPage, searchQuery } = this.state;
+    const { user } = this.props;
+
+    const searchPlaceHolder = "Search ...";
     return (
       <React.Fragment>
-        <div className="recipe-short-container">
-          {this.state.recipes.map((recipe, i) => (
-            <div
-              className="recipe-short-wrapper"
-              key={i}
-              onClick={this.handleClick}
-            >
-              <div className="recipe-short-image-container">
-                <img
-                  src={
-                    recipe.imageLink
-                      ? recipe.imageLink
-                      : require("../images/vegetables.svg")
-                  }
-                  className="cover"
-                />
-              </div>
-              <span className="profile-container">
-                <div className="profile-wrapper">
-                  {recipe.tasteProfile.map((desc, k) => (
-                    <item className="profile-item" key={k}>
-                      {desc}
-                    </item>
-                  ))}
-                </div>
-              </span>
-              <h2>{recipe.title}</h2>
-              <h3>Origin: {recipe.originCountry}</h3>
-            </div>
-          ))}
-        </div>
+        {/*Search*/}
+        <SearchBar
+          handleFormSubmit={this.searchRecipes}
+          countries={this.state.countries}
+        />
+        {/*Recipe List */}
+        <RecipeList recipes={this.state.recipes.data} />
+        <Pagination
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
