2aa30eff6ba629db87dff174d90be369e9e639af
GlennChon <glenn.chon@gmail.com>
2019-09-03 22:41:20 +0700

    refactored out single recipe card for cardlist

---
+++ src/components/recipeCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Img from "react-image";
+import vegetableImg from "../images/vegetables.svg";
+
+const RecipeCard = ({ recipe }) => {
+  const { image_link, taste_profile, title, origin_country } = { ...recipe };
+  return (
+    <React.Fragment>
+      <div className="recipe-short-image-container">
+        <Img src={[image_link, vegetableImg]} className="cover" />
+      </div>
+      <span className="profile-container">
+        <div className="profile-wrapper">
+          {taste_profile.map((desc, k) => (
+            <span className="profile-item" key={k}>
+              {desc}
+            </span>
+          ))}
+        </div>
+      </span>
+      <h3>{title}</h3>
+      <h4>Origin: {origin_country.name}</h4>
+    </React.Fragment>
+  );
+};
+
+export default RecipeCard;
