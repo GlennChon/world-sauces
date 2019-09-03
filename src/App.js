ca4f0949368cdd276cb405eb0e74666d3b7d0913
GlennChon <glenn.chon@gmail.com>
2019-09-03 22:45:47 +0700

    added routes

--- src/App.js
+++ src/App.js
@@ -10,19 +10,24 @@ import Home from "./components/home";
 // import Logo from "./components/logo";
 // import Users from "./components/users";
 import NavBar from "./components/navBar";
+
+import Recipes from "./components/recipes";
 import RecipeForm from "./components/recipeForm";
 // import Footer from "./components/footer";
 
 import Logout from "./components/logout";
-// import Recipes from "./components/recipes";
 import NotFound from "./components/notFound";
 // import Countries from "./components/countries";
 // import SearchBar from "./components/searchBar";
 import LoginForm from "./components/loginForm";
 // import RecipeForm from "./components/recipeForm";
+
+import Profile from "./components/profile";
 import ProfileForm from "./components/profileForm";
 import RegisterForm from "./components/registerForm";
 // import TasteProfiles from "./components/tasteProfiles";
+import ProtectedRoute from "./components/common/protectedRoute";
+
 // CSS
 import "./css/App.css";
 import "react-toastify/dist/ReactToastify.css";
@@ -32,6 +37,7 @@ class App extends Component {
 
   componentDidMount() {
     const user = auth.getCurrentUser();
+    console.log(user);
     this.setState({ user });
     console.log(user);
   }
@@ -49,9 +55,15 @@ class App extends Component {
           <Switch>
             <Route path="/login" component={LoginForm} />
             <Route path="/logout" component={Logout} />
-            <Route path="/recipe" component={RecipeForm} />
+            <ProtectedRoute path="/recipes/:id" component={RecipeForm} />
+            <Route
+              path="/recipes"
+              render={props => <Recipes {...props} user={this.state.user} />}
+            />
             <Route path="/not-found" component={NotFound} />
-            <Route path="/profile" component={ProfileForm} />
+
+            <Route path="/profile" component={Profile} />
+            <Route path="/profile/edit" component={ProfileForm} />
             <Route path="/register" component={RegisterForm} />
             <Route path="/" exact component={Home} />
             <Redirect from="/home" to="/" />
