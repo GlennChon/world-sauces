8f6989892a93c2512ba3b8518c00de32b718f159
GlennChon <glenn.chon@gmail.com>
2019-09-04 16:23:48 +0700

    replace recipe with recipeDisplay

---
+++ src/components/recipeDisplay.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class RecipeDisplay extends Display {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default RecipeDisplay;
