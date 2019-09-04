00c50e8b645f158123046e765e22de2817055d70
GlennChon <glenn.chon@gmail.com>
2019-09-04 16:25:39 +0700

    added likes to card

--- src/components/recipeCard.jsx
+++ src/components/recipeCard.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Img from "react-image";
 import vegetableImg from "../images/vegetables.svg";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
 const RecipeCard = ({ recipe }) => {
-  const { image_link, taste_profile, title, origin_country } = { ...recipe };
+  const { image_link, taste_profile, title, origin_country, likes } = {
+    ...recipe
+  };
   return (
     <React.Fragment>
       <div className="recipe-short-image-container">
         <Img src={[image_link, vegetableImg]} className="cover" />
+        <div className="likes">
+          <FontAwesomeIcon icon={faHeart} className="card-icon" />{" "}
+          <h5>{likes}</h5>
+        </div>
       </div>
       <span className="profile-container">
         <div className="profile-wrapper">
