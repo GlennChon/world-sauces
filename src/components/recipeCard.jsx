9f0092ec365e9bd63e57612af103e252c0aa1d7a
GlennChon <glenn.chon@gmail.com>
2019-09-07 01:02:30 +0700

    change default image

--- src/components/recipeCard.jsx
+++ src/components/recipeCard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Img from "react-image";
-import vegetableImg from "../images/vegetables.svg";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
@@ -9,11 +8,13 @@ const RecipeCard = ({ recipe }) => {
   const { image_link, taste_profile, title, origin_country, likes } = {
     ...recipe
   };
+  const defaultImg =
+    "https://upload.wikimedia.org/wikipedia/commons/a/a8/Emojione_BW_1F372.svg";
 
   return (
     <React.Fragment>
       <div className="recipe-short-image-container">
-        <Img src={[image_link, vegetableImg]} className="cover" />
+        <Img src={[image_link, defaultImg]} className="cover" />
         <div className="recipe-card-likes">
           <FontAwesomeIcon icon={faHeart} className="card-icon" />
           <h5>{likes}</h5>
