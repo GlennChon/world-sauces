0b1add64fae6a3fbc977ddec7cbd797367b22fce
GlennChon <glenn.chon@gmail.com>
2019-09-08 02:08:11 +0700

    added update, get, save like, and remove like

--- src/services/userService.js
+++ src/services/userService.js
@@ -10,3 +10,31 @@ export function register(user) {
     username: user.username
   });
 }
+
+export function getSelfInfoById(id) {
+  return http.get(apiEndpoint + "/me", {
+    _id: id
+  });
+}
+
+export function getUserInfoById(id) {
+  return http.get(apiEndpoint + "/" + id);
+}
+
+export function updateUser(user) {
+  return http.put(apiEndpoint, {
+    _id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    about: user.about,
+    email: user.email
+  });
+}
+
+export function saveLike(userId, recipeId) {
+  return http.put(apiEndpoint + "/like", { _id: userId, recipeId });
+}
+
+export function removeLike(userId, recipeId) {
+  return http.put(apiEndpoint + "/unlike", { _id: userId, recipeId });
+}
