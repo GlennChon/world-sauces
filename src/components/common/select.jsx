3a891016f2bee174bde58c2f4be098903cf8e3c3
GlennChon <glenn.chon@gmail.com>
2019-09-08 02:07:48 +0700

    added disabled to select country

--- src/components/common/select.jsx
+++ src/components/common/select.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
-const Select = ({ name, label, options, error, ...rest }) => {
+const Select = ({ name, label, options, error, isDisabled, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <select name={name} id={name} {...rest} className="form-control">
+      <select
+        name={name}
+        id={name}
+        {...rest}
+        className="form-control"
+        disabled={isDisabled}
+      >
         <option value="" />
         {options.map(option => (
-          <option key={option.code} value={option.code}>
+          <option key={option.code} value={option.name}>
             {option.name}
           </option>
         ))}
