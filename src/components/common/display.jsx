0d82293f7917162d477c49dc94b2615054a21831
GlennChon <glenn.chon@gmail.com>
2019-09-16 19:20:01 +0700

    switched icon

--- src/components/common/display.jsx
+++ src/components/common/display.jsx
@@ -36,7 +36,7 @@ class Display extends Component {
   };
 
   renderLikes = (likes = 0, isLiked = false) => {
-    let icon = isLiked ? heartSolid : heartRegular;
+    let icon = !isLiked ? heartSolid : heartRegular;
     likes = this.state.data.likes;
     return (
       <React.Fragment>
