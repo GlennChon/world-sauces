19c86a5e0c8a490378b2b0a47f279595a3ce1072
GlennChon <glenn.chon@gmail.com>
2019-09-25 16:26:45 +0700

    simplified about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,76 +7,71 @@ class About extends Component {
     return (
       <>
         <Container>
-          <Row className="justify-content-md-center">
-            <Col xs="0" lg="2" />
-            <Col md="auto">
-              <h1>About</h1>
-            </Col>
-            <Col xs="0" lg="2" />
-          </Row>
+          <header>
+            <Row className="justify-content-around">
+              <h3>Sharing</h3>
+            </Row>
+          </header>
+          <section>
+            <Row className="justify-content-sm-center">
+              <Col xs={0} md={1} lg={2} />
+              <Col xs={12} md={10} lg={8}>
+                <p>
+                  Over years of travel and sharing meals around the world, I
+                  have noticed that each country has their own sauces that are
+                  rarely represented outside of their respective countries.
+                </p>
+              </Col>
+              <Col xs={0} md={1} lg={2} />
+            </Row>
+          </section>
 
-          <Row className="justify-content-md-center">
-            <Col xs="0" lg="2" />
-            <Col md="auto">
-              <p>
-                Over years of travel, I have noticed that each country has their
-                own sauces that are rarely represented outside of their
-                respective countries. My love for cooking and coding has led me
-                to develop a sauce database to help others learn about these
-                amazing sauces and open up an avenue for people to share their
-                recipes from around the world. I hope you will join me in
-                growing and sharing this website with all those who love food.
-              </p>
-            </Col>
-            <Col xs="0" lg="2" />
-          </Row>
+          <header>
+            <Row className="justify-content-around">
+              <h3>Cooking &amp; Coding</h3>
+            </Row>
+          </header>
+          <section>
+            <Row className="justify-content-sm-center">
+              <Col xs={0} md={1} lg={2} />
+              <Col xs={12} md={10} lg={8}>
+                <p>
+                  My love for cooking and coding has led me to develop a sauce
+                  database to help others learn about these amazing sauces and
+                  open up an avenue for people to share their recipes from
+                  around the world.
+                  <br />I hope you will join me in growing this website, and I
+                  hope this website will enable you to share with those around
+                  you.
+                </p>
+              </Col>
+              <Col xs={0} md={1} lg={2} />
+            </Row>
+          </section>
 
-          <Row className="justify-content-md-center">
-            <Col xs="0" lg="2" />
-            <Col md="auto">
-              <h3>What I believe</h3>
-            </Col>
-            <Col xs="0" lg="2" />
-          </Row>
+          <header>
+            <Row className="justify-content-around">
+              <h3>What you can do</h3>
+            </Row>
+          </header>
 
-          <Row className="justify-content-md-center">
-            <Col xs="0" lg="3" />
-            <Col md="auto">
-              <p>
-                I believe sharing great food brings people together and I hope
-                the recipes you find here will enable you to share with those
-                around you.
-              </p>
-            </Col>
-            <Col xs="0" lg="3" />
-          </Row>
-
-          <Row className="justify-content-md-center">
-            <Col xs="0" lg="2" />
-            <Col md="auto">
-              <h3>Here are a few ways you can help this website!</h3>
-            </Col>
-            <Col xs="0" lg="2" />
-          </Row>
-
-          <Row className="justify-content-md-center">
-            <Col xs="0" lg="3" />
-            <Col md="auto">
-              <ListGroup>
+          <section>
+            <Row className="justify-content-around">
+              <ListGroup className="align-items-center">
                 <ListGroupItem>
                   Use the referral links here to purchase anything you may need.
+                  (Coming Soon)
                 </ListGroupItem>
                 <ListGroupItem>
-                  Register and add Recipes to grow the database.
+                  Register and add recipes to grow the site.
                 </ListGroupItem>
                 <ListGroupItem>Share the site with your friends.</ListGroupItem>
                 <ListGroupItem>
-                  Report any bugs through the contact form.
+                  Report any bugs through the contact form. (Coming Soon)
                 </ListGroupItem>
               </ListGroup>
-            </Col>
-            <Col xs="0" lg="3" />
-          </Row>
+            </Row>
+          </section>
         </Container>
       </>
     );
