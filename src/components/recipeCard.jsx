68c058f072fb908fcc7a1652b43b28ff01c814fb
GlennChon <glenn.chon@gmail.com>
2019-09-29 14:30:55 +0700

    added visibility sensor to recipe cards

--- src/components/recipeCard.jsx
+++ src/components/recipeCard.jsx
@@ -4,8 +4,9 @@ import Img from "react-image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { defaultImg, loaderImg } from "../config.json";
-import { Row, Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
+import VisibilitySensor from "react-visibility-sensor";
 import "../css/recipeCard.css";
 
 const RecipeCard = ({ recipe }) => {
@@ -15,54 +16,63 @@ const RecipeCard = ({ recipe }) => {
 
   return (
     <React.Fragment>
-      <a
-        style={{ "text-decoration": "none" }}
-        href={"/recipe/" + recipe._id}
-        className="recipe-card"
-      >
-        <section>
-          <div className="recipe-card-image-container" aria-label="recipe card">
-            <Img
-              src={image_link}
-              alt={title + " Image"}
-              className="cover"
-              loader={
-                <img
-                  src={window.location.origin + loaderImg}
+      <VisibilitySensor>
+        {({ isVisible }) => (
+          <a
+            style={{ "text-decoration": "none" }}
+            href={"/recipe/" + recipe._id}
+            className="recipe-card"
+          >
+            <section>
+              <div
+                className="recipe-card-image-container"
+                aria-label="recipe card"
+              >
+                <Img
+                  src={image_link}
+                  alt={title + " Image"}
                   className="cover"
-                  alt="sauce"
+                  loader={
+                    <img
+                      src={window.location.origin + loaderImg}
+                      className="cover"
+                      alt="sauce"
+                    />
+                  }
+                  unloader={
+                    <img src={defaultImg} className="cover" alt="placeholder" />
+                  }
                 />
-              }
-              unloader={
-                <img src={defaultImg} className="cover" alt="placeholder" />
-              }
-            />
-            <div className="recipe-card-likes">
-              <FontAwesomeIcon icon={faHeart} className="card-icon" />
-              <h5>{likes}</h5>
-            </div>
-          </div>
-        </section>
-        <section>
-          <Row className="justify-content-around">
-            <div className="card-profile-wrapper">
-              {taste_profile.map((desc, k) => (
-                <span className="card-profile-item" key={k}>
-                  {desc}
-                </span>
-              ))}
-            </div>
-          </Row>
+                <div className="recipe-card-likes">
+                  <FontAwesomeIcon icon={faHeart} className="card-icon" />
+                  <h5>{likes}</h5>
+                </div>
+              </div>
+            </section>
+            <section>
+              <Row className="justify-content-around">
+                <div className="card-profile-wrapper">
+                  {taste_profile.map((desc, k) => (
+                    <span className="card-profile-item" key={k}>
+                      {desc}
+                    </span>
+                  ))}
+                </div>
+              </Row>
 
-          <Row className="justify-content-around">
-            <h3>{title}</h3>
-          </Row>
+              <Row className="justify-content-around">
+                <h3>{title}</h3>
+              </Row>
 
-          <Row className="justify-content-around">
-            <h5>Origin: {origin_country == null ? " ~ " : origin_country}</h5>
-          </Row>
-        </section>
-      </a>
+              <Row className="justify-content-around">
+                <h5>
+                  Origin: {origin_country == null ? " ~ " : origin_country}
+                </h5>
+              </Row>
+            </section>
+          </a>
+        )}
+      </VisibilitySensor>
     </React.Fragment>
   );
 };
