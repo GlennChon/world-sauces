47affa91e75955248e6e0e6f24414fc37f7c9789
glennchon <glenn.chon@gmail.com>
2019-11-02 14:19:42 +0700

    put formcontrol and error in a column

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, Col } from "react-bootstrap";
 
 const Input = ({ name, label, error, ...rest }) => {
   return (
-    <React.Fragment>
+    <Col>
       <Form.Control {...rest} name={name} id={name} />
       {error && <div className="alert alert-danger">{error}</div>}
-    </React.Fragment>
+    </Col>
   );
 };
 
