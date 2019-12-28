21ae98135d6f2952a86ed7346623d92e927aa746
glennchon <glenn.chon@gmail.com>
2019-12-28 14:12:00 +0700

    changed location of some imports

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import HttpsRedirect from "react-https-redirect";
 // Services
 import ReactGA from "react-ga";
 import auth from "./services/authService";
-// Components
-import Home from "./components/home";
+
+// Pages
+import Home from "./components/pages/home";
+import Recipes from "./components/pages/recipes";
+import AddRecipe from "./components/pages/addRecipe";
+// import Footer from "./components/footer";
+import Logout from "./components/pages/logout";
+import Login from "./components/pages/login";
+// import About from "./pages/about";
+// import NotFound from "./components/notFound";
+// import ProfileForm from "./components/profileForm";
+// import RegisterForm from "./components/registerForm";
+// import RecipeDisplay from "./components/recipeDisplay";
+// import ProfileDisplay from "./components/profileDisplay";
+import ProtectedRoute from "./components/common/protectedRoute";
+//Components
 // import Logo from "./components/logo";
 // import Users from "./components/users";
 import NavBar from "./components/navBar";
-import Recipes from "./components/recipes";
-// import Footer from "./components/footer";
-import About from "./components/about";
-import Logout from "./components/logout";
-import NotFound from "./components/notFound";
-import LoginForm from "./components/loginForm";
-import RecipeForm from "./components/recipeForm";
-import ProfileForm from "./components/profileForm";
-import RegisterForm from "./components/registerForm";
-import RecipeDisplay from "./components/recipeDisplay";
-import ProfileDisplay from "./components/profileDisplay";
-import ProtectedRoute from "./components/common/protectedRoute";
 
 import "./css/App.css";
 
@@ -49,22 +51,11 @@ class App extends Component {
           <header className="page-header header container-fluid"></header>
           <main className="container-fluid">
             <Switch>
+              <Route path="/recipe/edit/:id" component={AddRecipe} />
               <Route path="/logout" component={Logout} />
-              <Route path="/login" component={LoginForm} />
-              <Route path="/about" component={About} />
-              <Route path="/not-found" component={NotFound} />
-
-              <ProtectedRoute path="/recipe/edit/:id" component={RecipeForm} />
-              <Route path="/recipe/:id" component={RecipeDisplay} />
-              <Route path="/recipes" component={Recipes} />
-
-              <ProtectedRoute path="/profile/me" component={ProfileForm} />
-              <Route path="/profile/:username" component={ProfileDisplay} />
-
-              <Route path="/register" component={RegisterForm} />
-              <Route path="/" exact component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/" component={Home} />
               <Redirect from="/home" to="/" />
-              <Redirect to="/not-found" />
             </Switch>
           </main>
         </React.Fragment>
@@ -72,5 +63,18 @@ class App extends Component {
     );
   }
 }
+// <Route path="/about" component={About} />
+//               <Route path="/not-found" component={NotFound} />
 
+//               <ProtectedRoute path="/recipe/edit/:id" component={RecipeForm} />
+//               <Route path="/recipe/:id" component={RecipeDisplay} />
+//               <Route path="/recipes" component={Recipes} />
+
+//               <ProtectedRoute path="/profile/me" component={ProfileForm} />
+//               <Route path="/profile/:username" component={ProfileDisplay} />
+
+//               <Route path="/register" component={RegisterForm} />
+//               <Route path="/" exact component={Home} />
+//
+//               <Redirect to="/not-found" />
 export default App;
